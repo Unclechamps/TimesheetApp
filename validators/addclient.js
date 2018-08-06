25c874a05d3b59095edcdde673b9da6305d7bc54
Gabe <36457852+Unclechamps@users.noreply.github.com>
2018-08-05 20:28:26 -0500

    heroku

--- validators/addclient.js
+++ validators/addclient.js
@@ -1,5 +1,5 @@
 const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const isEmpty = require('./isempty');
 
 module.exports = function validateClientInput(data) {
   let errors = {};
@@ -29,4 +29,4 @@ module.exports = function validateClientInput(data) {
     errors,
     isValid: isEmpty(errors)
   };
-}; 
+};
